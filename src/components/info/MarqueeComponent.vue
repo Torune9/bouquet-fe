3fcb133ec9a7a903ceaaa75a40df663357be74af
rahman <rahmannurhayadi@gmail.com>
2025-03-10 11:19:33 +0700

    feat(component) : add marquee component

---
+++ src/components/info/MarqueeComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <section
+        class="text-orioles-linen  w-screen h-20 flex flex-row justify-center items-center bg-neutral uppercase overflow-hidden border relative">
+        <div class="py-12 animate-marquee whitespace-nowrap">
+            <span class="text-4xl mx-4">
+                amazing flower crafts
+            </span>
+            <span class="text-4xl mx-4">
+                Snf Bouquet
+            </span>
+            <span class="text-4xl mx-4">
+                amazing flower crafts
+            </span>
+            <span class="text-4xl mx-4">
+                Snf Bouquet
+            </span>
+            <span class="text-4xl mx-4">
+                amazing flower crafts
+            </span>
+        </div>
+
+        <div class="absolute top-0 py-5 animate-marquee2 whitespace-nowrap">
+            <span class="text-4xl mx-4">
+                amazing flower crafts
+            </span>
+            <span class="text-4xl mx-4">
+                Snf Bouquet
+            </span>
+            <span class="text-4xl mx-4">
+                amazing flower crafts
+            </span>
+            <span class="text-4xl mx-4">
+                Snf Bouquet
+            </span>
+            <span class="text-4xl mx-4">
+                amazing flower crafts
+            </span>
+        </div>
+    </section>
+</template>
