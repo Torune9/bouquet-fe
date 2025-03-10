209d989da3400957e6af996c55f284cf08f83b02
rahman <rahmannurhayadi@gmail.com>
2025-03-10 11:19:47 +0700

    feat(layouts) : add base layout

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+        header
+    </header>
+
+    <main class="font-archivo">
+        <slot name="content" />
+    </main>
+
+    <footer>
+        footer
+    </footer>
+
+</template>
