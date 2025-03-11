cec5038b5e408d3fd45bfabaa8459a7e0d7f8433
rahman <rahmannurhayadi@gmail.com>
2025-03-11 09:29:14 +0700

    setup font awesome icon

--- src/main.js
+++ src/main.js
@@ -7,8 +7,19 @@ import router from './router'
 
 import { pinia } from './services/pinia'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+
+library.add(fas,fab,far)
+
 
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(pinia)
 app.use(router)
