99af3d6bc08d0dfa971c71f55e7e966c6f0d2a93
rahman <rahmannurhayadi@gmail.com>
2025-03-12 09:34:17 +0700

    style(view) : fix color marquee

---
+++ src/views/MainPageView.vue
@@ -0,0 +1,24 @@
+<template>
+   <MainLayout>
+    <template #content>
+        <HeroSection/>
+
+        <MarqueeComponent/>
+
+        <CategorySection class="mt-4"/>
+
+        <ProductSection class="mt-4"/>
+
+        <MarqueeComponent class="bg-orioles-orange text-orioles-linen"/>
+    </template>
+   </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '@/layouts/MainLayout.vue'
+import MarqueeComponent from '@/components/info/MarqueeComponent.vue';
+
+import HeroSection from '@/components/landingpage/HeroSection.vue';
+import CategorySection from '@/components/landingpage/CategorySection.vue';
+import ProductSection from '@/components/landingpage/ProductSection.vue';
+</script>
