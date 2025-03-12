45c19cd7c708bccd9ff306aef5061973c92fa8a8
rahman <rahmannurhayadi@gmail.com>
2025-03-12 09:33:20 +0700

    feat : add path bouquet

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MainPageView from '@/views/MainPageView.vue'
+import DetailView from '@/views/DetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/bouquet',
+      name: 'bouquet',
+      component: MainPageView,
+    },
+    {
+      path: '/bouquet/:id',
+      name: 'detail-bouquet',
+      component: DetailView,
     },
   ],
 })
