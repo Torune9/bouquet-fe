77f28b03eeab85c079969b3199e15cc1b4041f6c
rahman <rahmannurhayadi@gmail.com>
2025-03-12 09:44:52 +0700

    fix(layout) : fix footer import

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,16 +2,15 @@
 
     <NavBar/>
     
-    <main class="font-archivo">
+    <main class="font-archivo overflow-x-hidden">
         <slot name="content" />
     </main>
 
-    <footer>
-        footer
-    </footer>
+    <Footer/>
 
 </template>
 
 <script setup>
+import Footer from '@/components/navigation/Footer.vue';
 import NavBar from '@/components/navigation/NavBar.vue';
 </script>
