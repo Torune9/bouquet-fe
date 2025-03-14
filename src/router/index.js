fe1f9edba8b3e4f0bfaed83df8b6286b9f485cf5
rahman <rahmannurhayadi@gmail.com>
2025-03-14 10:03:57 +0700

    feat(router) : add notfound,login,register router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import MainPageView from '@/views/MainPageView.vue'
-import DetailView from '@/views/DetailView.vue'
+import MainPageView from '@/views/mainPage/MainPageView.vue'
+import DetailView from '@/views/mainPage/DetailView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import LoginView from '@/views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +13,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path : '/sign-in',
+      name : 'login',
+      component : LoginView
+    },
+    {
+      path : '/sign-up',
+      name : 'register',
+      component : RegisterView
     },
     {
       path: '/bouquet',
@@ -21,6 +34,10 @@ const router = createRouter({
       name: 'detail-bouquet',
       component: DetailView,
     },
+    { 
+      path: '/:pathMatch(.*)*', 
+      component: NotFoundView 
+    }
   ],
 })
 
