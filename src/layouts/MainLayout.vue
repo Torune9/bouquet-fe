be40219f4e828aa7fa7a708ffa700f36d9835a4a
rahman <rahmannurhayadi@gmail.com>
2025-03-14 10:02:10 +0700

    featlayout) : add show cart function

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,9 @@
 <template>
+    <NavBar @show-cart="showCart"/>
 
-    <NavBar/>
-    
-    <main class="font-archivo overflow-x-hidden">
+    <CartComponent :is-show-cart="isShowCart"/>
+
+    <main class="font-archivo overflow-x-hidden relative">
         <slot name="content" />
     </main>
 
@@ -11,6 +12,14 @@
 </template>
 
 <script setup>
+import CartComponent from '@/components/landingpage/CartComponent.vue';
 import Footer from '@/components/navigation/Footer.vue';
 import NavBar from '@/components/navigation/NavBar.vue';
+import { ref } from 'vue';
+
+const isShowCart = ref(false)
+
+const showCart = ()=>{
+    isShowCart.value = !isShowCart.value
+}
 </script>
