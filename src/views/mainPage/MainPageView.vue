28d6a726487e43dcb305fbcc7e5331465ee9e357
rahman <rahmannurhayadi@gmail.com>
2025-03-14 10:06:10 +0700

    refactor(view) : change dir mainPage

---
+++ src/views/mainPage/MainPageView.vue
@@ -0,0 +1,22 @@
+<template>
+   <MainLayout>
+    <template #content>
+        <HeroSection/>
+
+        <MarqueeComponent/>
+
+        <CategorySection class="mt-4"/>
+
+        <ProductSection class="mt-4"/>
+    </template>
+   </MainLayout>
+</template>
+
+<script setup>
+import MainLayout from '@/layouts/MainLayout.vue'
+import MarqueeComponent from '@/components/info/MarqueeComponent.vue';
+
+import HeroSection from '@/components/landingpage/HeroSection.vue';
+import CategorySection from '@/components/landingpage/CategorySection.vue';
+import ProductSection from '@/components/landingpage/ProductSection.vue';
+</script>
