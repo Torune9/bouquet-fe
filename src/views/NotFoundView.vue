23fbb78a80b3b7410e8b6dbebb28bdde0c20ba02
rahman <rahmannurhayadi@gmail.com>
2025-03-14 10:04:13 +0700

    feat(view) : add not founf view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="h-screen w-screen flex justify-center items-center flex-col gap-y-4">
+        <h1 class="text-9xl font-black">404</h1>
+        <h2>Page Not Found</h2>
+        <p>Oops looks like the page doesn't exist</p>
+        <button>
+            <RouterLink to="/" class="btn">
+                Home
+            </RouterLink>
+        </button>
+    </div>
+</template>
