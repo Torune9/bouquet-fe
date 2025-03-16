54b19540dc85d2613e713c6749e6cb5dd93207b5
rahman <rahmannurhayadi@gmail.com>
2025-03-16 10:46:52 +0700

    feat(component) : add navigation admin

---
+++ src/components/admin/NavBarAdmin.vue
@@ -0,0 +1,34 @@
+<template>
+   <div class="flex items-center justify-between p-4">
+    <!-- Toggle Button for Mobile -->
+    <button 
+      @click="toggleSidebar"
+      class="lg:hidden text-gray-600 hover:text-gray-800 cursor-pointer"
+    >
+      <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+      </svg>
+    </button>
+
+    <!-- Title -->
+    <h2 class="text-xl font-semibold text-gray-800">Dashboard</h2>
+
+    <!-- Menu Items -->
+    <div class="flex items-center space-x-4">
+      <button class="text-gray-600 hover:text-gray-800">
+        Notifications
+      </button>
+      <button class="text-gray-600 hover:text-gray-800">
+        Profile
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const emits = defineEmits(['toggleSidebar'])
+const toggleSidebar = ()=>{
+    return emits('toggleSidebar')
+}
+
+</script>
---
+++ src/components/admin/SidebarAdmin.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="p-6">
+        <h1 class="text-2xl font-bold text-gray-800">Admin Panel</h1>
+        <nav class="mt-6">
+            <ul class="flex flex-col gap-y-1">
+                <li v-for="(menu, i) in menus" :key="i">
+                    <router-link :to="menu.routes"
+                        class="flex items-center p-4 rounded-md text-gray-600 hover:bg-neutral hover:text-orioles-linen transition-colors duration-300 gap-x-4"
+                        active-class="bg-indigo-900/30 text-indigo-900">
+                        <span class="w-6 inline-flex justify-center items-center text-xl"><font-awesome-icon :icon="menu.icon" /></span>
+                        <span>{{ menu.name }}</span>
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+const menus = [
+    {
+        name: 'Dashboard',
+        icon: 'fa-solid fa-globe',
+        routes: '/admin/dashboard'
+    },
+    {
+        name: 'Category',
+        icon: 'fa-solid fa-arrows-to-circle',
+        routes: '/admin/category'
+    },
+    {
+        name: 'Bouquets',
+        icon: 'fa-solid fa-seedling',
+        routes: '/admin/bouquet'
+    },
+    {
+        name: 'Order',
+        icon: 'fa-solid fa-bullseye',
+        routes: '/admin/order'
+    },
+    {
+        name: 'User',
+        icon: 'fa-solid fa-people-group',
+        routes: '/admin/user'
+    },
+    {
+        name: 'Role',
+        icon: 'fa-solid fa-arrows-spin',
+        routes: '/admin/role'
+    },
+]
+
+</script>
