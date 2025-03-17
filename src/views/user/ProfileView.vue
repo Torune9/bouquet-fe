216c56eb0e775c0c587c84d8b30301d37b8b8f07
rahman <rahmannurhayadi@gmail.com>
2025-03-17 09:01:40 +0700

    style(view) : fix layout and bg color profile

--- src/views/user/ProfileView.vue
+++ src/views/user/ProfileView.vue
@@ -4,29 +4,36 @@
             <div
                 class="grid grid-cols-1 grid-rows-[auto_1fr] w-full p-4 lg:px-28  md:px-24 sm:px-16 lg:grid-cols-[300px_1fr] lg:gap-4">
                 <div
-                    class="grid grid-cols-[120px_1fr] grid-rows-[1fr_auto] max-md:justify-center gap-2 lg:gap-4 lg:flex lg:flex-col lg:p-4 lg:items-center p-2 bg-neutral text-orioles-linen rounded-md h-fit font-light">
-                    <div
-                        class="w-24 h-24 lg:w-40 lg:h-40 rounded-full overflow-hidden max-md:justify-self-center max:mdself-center">
+                    class="grid grid-cols-[120px_1fr] grid-rows-[1fr_auto] max-md:justify-center gap-2 lg:gap-4 lg:flex lg:flex-col lg:p-4 lg:items-center p-2 h-fit shadow">
+                    <div class="w-24 h-24 lg:w-40 lg:h-40 rounded-full overflow-hidden max-md:justify-self-center">
                         <img :src="img" alt="" class="w-full h-full object-cover">
                     </div>
-                    <div class="flex flex-col gap-y-1 justify-center w-full text-sm">
-                        <p><span class="font-bold w-20 inline-flex justify-between">Username<span>:</span></span>&nbsp;Jhon doe</p>
-                        <p><span class="font-bold w-20 inline-flex justify-between">Email<span>:</span></span>&nbsp;jhondoe@gmail.com</p>
-                        <p><span class="font-bold w-20 inline-flex justify-between">Phone<span>:</span></span>&nbsp;+72 888-9998-0000</p>
-                    </div>
-                    <div class="inline-flex text-sm col-span-2 gap-x-4 lg:flex lg:flex-col lg:gap-y-4">
-                        <div class="w-48 md:w-28 lg:inline-flex lg:gap-x-4 lg:w-full">
-                            <p class="text-info">Total Order : 100</p>
-                            <p class="text-warning"><font-awesome-icon icon="fa-solid fa-cart-shopping" /> 10</p>
-                        </div>
+                    <div class="flex flex-col gap-y-4 justify-center w-full text-xs sm:text-lg">
                         <div>
-                            <h1 class=" text-orioles-orange font-bold">Active address</h1>
-                            <ul class="text-xs list-disc font-light list-inside">
-                                <li class="">
-                                    Adress Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, cum?
-                                </li>
-                            </ul>
+                            <p>
+                                <span class="w-16 sm:w-24 inline-flex justify-between">
+                                    Username
+                                    <span>:</span>
+                                </span>&nbsp;Jhondoe
+                            </p>
+                            <p>
+                                <span class="w-16 sm:w-24 inline-flex justify-between">
+                                    Email
+                                    <span>:</span>
+                                </span>&nbsp;jhondoe@gmail.com
+                            </p>
+                            <p>
+                                <span class="w-16 sm:w-24 inline-flex justify-between">
+                                    Phone
+                                    <span>:</span>
+                                </span>&nbsp;+72 888-9998-0000
+                            </p>
                         </div>
+                        <ul class="text-xs font-light">
+                            <li>
+                                Adress Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, cum?
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <!-- Infomation -->
@@ -56,7 +63,7 @@
                     <form action="#" class="inline-flex items-center mt-8 w-full gap-x-2">
                         <div class="relative w-full">
                             <label for="address" class="absolute bottom-10">Address</label>
-                            <input type="text" class="input" id="address" name="address">
+                            <input type="text" class="input w-full" id="address" name="address">
                         </div>
                         <button class="btn">add</button>
                     </form>
