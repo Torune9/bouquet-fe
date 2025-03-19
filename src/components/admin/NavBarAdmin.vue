84aaca1c7384a4c369d03a7343d75abd30246e86
rahman <rahmannurhayadi@gmail.com>
2025-03-19 15:35:07 +0700

    fix(component) : add profile admin in navbar admin

--- src/components/admin/NavBarAdmin.vue
+++ src/components/admin/NavBarAdmin.vue
@@ -1,10 +1,7 @@
 <template>
-   <div class="flex items-center justify-between p-4">
+  <div class="flex items-center justify-between p-4">
     <!-- Toggle Button for Mobile -->
-    <button 
-      @click="toggleSidebar"
-      class="lg:hidden text-gray-600 hover:text-gray-800 cursor-pointer"
-    >
+    <button @click="toggleSidebar" class="lg:hidden text-gray-600 hover:text-gray-800 cursor-pointer">
       <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
       </svg>
@@ -14,21 +11,41 @@
     <h2 class="text-xl font-semibold text-gray-800">Dashboard</h2>
 
     <!-- Menu Items -->
-    <div class="flex items-center space-x-4">
-      <button class="text-gray-600 hover:text-gray-800">
-        Notifications
+    <div class="flex items-center space-x-8">
+        <button class="text-gray-600 hover:text-gray-800 indicator">
+          <font-awesome-icon icon="fa-solid fa-store" size="lg"/>
+          <span class="badge badge-sm indicator-item">8</span>
       </button>
-      <button class="text-gray-600 hover:text-gray-800">
-        Profile
-      </button>
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost btn-lg btn-circle avatar">
+          <div class="w-20 rounded-full">
+            <img alt="Tailwind CSS Navbar component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+          </div>
+        </div>
+        <ul tabindex="0" class="menu menu-md dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+          <li>
+            <RouterLink :to="{name : 'profile-admin'}" class="justify-between">
+              Profile
+            </RouterLink>
+          </li>
+          <li @click="logout"><a>Logout</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { useAuthStore } from '@/stores/authStore'
+
 const emits = defineEmits(['toggleSidebar'])
-const toggleSidebar = ()=>{
-    return emits('toggleSidebar')
+const toggleSidebar = () => {
+  return emits('toggleSidebar')
 }
 
+const authStore = useAuthStore()
+const logout = ()=>{
+  authStore.logout()
+}
 </script>
