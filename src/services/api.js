4259dcbe220e24b5f5e4664080a1a2fb78a44709
rahman <rahmannurhayadi@gmail.com>
2025-03-20 12:25:56 +0700

    feat(service) : add req interceptor api

---
+++ src/services/api.js
@@ -0,0 +1,47 @@
+import { useAuthStore } from "@/stores/authStore"
+import axios from "axios"
+const BASE_URL = import.meta.env.VITE_BASE_APP_URL
+
+export const api = axios.create({
+    baseURL : BASE_URL,
+})
+
+
+api.interceptors.request.use(
+    (config) => {
+        console.log('Intercepting request...');
+
+        const token = useAuthStore()?.token;
+        const protectedEndpoints = [
+            '/profile',
+            '/address',
+            '/role'
+        ];
+
+        const restrictedEndpoints = [
+            '/order',
+            '/bouquet',
+            '/category'
+        ];
+
+        let needsAuth = protectedEndpoints.some(endpoint => config.url.startsWith(endpoint));
+
+        if (!needsAuth) {
+            needsAuth = restrictedEndpoints.some(endpoint => 
+                config.url.startsWith(endpoint) && config.method.toLowerCase() !== 'get'
+            );
+        }
+
+        console.log(`Needs Auth: ${needsAuth}, URL: ${config.url}, Method: ${config.method}`);
+
+        if (needsAuth && token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
