2f639a77b9637f4e2aa6fa6bf8299f08fe639e14
rahman <rahmannurhayadi@gmail.com>
2025-03-23 11:04:30 +0700

    fix(store) : set category in localstorage

--- src/stores/categoryStore.js
+++ src/stores/categoryStore.js
@@ -3,12 +3,14 @@ import { defineStore } from "pinia";
 
 export const useCategoryStore = defineStore('categories',{
     state : ()=>({
-        
+        categories : []
     }),
+    persist : true,
     actions : {
         async getCategories (){
             try {
                 const response = await api.get('/category')
+                this.categories = [...response.data.data]
                 return response.data
             } catch (error) {
                 throw error
