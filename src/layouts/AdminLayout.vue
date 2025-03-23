44ba1cb7fdf8c950fdf060a9b472aa562047b649
rahman <rahmannurhayadi@gmail.com>
2025-03-23 11:02:08 +0700

    fix(layout) : fix get Category

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="min-h-screen bg-gray-100 flex font-archivo">
+  <div class="flex bg-gray-100 font-archivo min-h-screen">
     <!-- Sidebar (Hidden on Mobile) -->
-    <aside :class="{ '-translate-x-full': !isSidebarOpen }"
-      class="w-64 bg-white shadow-lg fixed h-full transform transition-transform duration-300 ease-in-out lg:translate-x-0 z-30">
+    <aside :class="{'-translate-x-full': !isSidebarOpen }"
+      class="bg-white h-full shadow-lg w-64 duration-300 ease-in-out fixed lg:translate-x-0 transform transition-transform z-30">
       <Sidebar />
     </aside>
 
     <!-- Overlay for Mobile (Hidden on Desktop) -->
-    <div v-if="isSidebarOpen" @click="isSidebarOpen = false" class="fixed inset-0 bg-black/50 z-20 lg:hidden" />
+    <div v-if="isSidebarOpen" @click="isSidebarOpen = false" class="bg-black/50 fixed inset-0 lg:hidden z-20" />
 
     <!-- Main Content -->
-    <div class="flex-1 flex flex-col overflow-hidden lg:ml-64">
+    <div class="flex flex-1 flex-col lg:ml-64 overflow-hidden">
       <!-- Navbar -->
       <header class="bg-white shadow-lg">
         <Navbar @toggle-sidebar="isSidebarOpen = !isSidebarOpen" />
       </header>
 
       <!-- Dynamic Content -->
-      <main class="flex-1 overflow-y-auto p-2 sm:p-6 bg-gray-100">
+      <main class="flex-1 bg-gray-100 p-2 overflow-y-auto sm:p-6">
         <router-view />
       </main>
     </div>
@@ -27,6 +27,18 @@
 <script setup>
 import Sidebar from '@/components/admin/SidebarAdmin.vue'
 import Navbar from '@/components/admin/NavBarAdmin.vue'
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { useCategoryStore } from '@/stores/categoryStore';
 const isSidebarOpen = ref(false)
+const categoryStore = useCategoryStore()
+const getCategories = async ()=>{
+  try {
+    await categoryStore.getCategories()
+  } catch (error) {
+    console.log(error);
+  }
+}
+onMounted(async()=>{
+  await getCategories()
+})
 </script>
