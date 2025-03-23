3e1e5dbf0a2850cc1e82e45be5f0a16f4a1d80eb
rahman <rahmannurhayadi@gmail.com>
2025-03-23 11:02:30 +0700

    feat(view) : add btn action in category admin view

--- src/views/admin/categories/CategoryView.vue
+++ src/views/admin/categories/CategoryView.vue
@@ -3,7 +3,7 @@
         <BtnSearch />
 
         <section>
-            <EasyDataTable :headers="headers" :items="items" :rows-items="[8, 12]" :rows-per-page="8"
+            <EasyDataTable :headers="headers" :items="categories" :rows-items="[8, 12]" :rows-per-page="8"
                 table-class-name="customize-table" :loading="isLoading">
                 <template #loading>
                     <span class="loading loading-bars loading-lg"></span>
@@ -12,11 +12,11 @@
                    <span class="capitalize">{{ name }}</span>
                 </template>
                 <template #item-action>
-                    <div class="inline-flex gap-x-2">
-                        <button class="btn btn-circle btn-sm btn-info">
+                    <div class="gap-x-2 inline-flex">
+                        <button class="btn btn-circle btn-info btn-sm">
                             <font-awesome-icon icon="fa-solid fa-pen-to-square" />
                         </button>
-                        <button class="btn btn-circle btn-sm btn-error">
+                        <button class="btn btn-circle btn-error btn-sm">
                             <font-awesome-icon icon="fa-solid fa-xmark" />
                         </button>
                     </div>
@@ -29,8 +29,10 @@
 <script setup>
 import BtnSearch from '@/components/admin/BtnSearch.vue';
 import { useCategoryStore } from '@/stores/categoryStore';
-import { onMounted, ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
 const categoryStore = useCategoryStore()
+
 const headers = [
     {
         text: 'Name',
@@ -46,21 +48,8 @@ const headers = [
     }
 ];
 
-const items = ref([])
+const {categories} = storeToRefs(categoryStore)
+
 const isLoading = ref(false)
 
-const getCategories = async () => {
-    isLoading.value = !isLoading.value
-    try {
-        const response = await categoryStore.getCategories()
-        items.value = response.data
-    } catch (error) {
-        console.log(error);
-    } finally {
-        isLoading.value = !isLoading.value
-    }
-}
-onMounted(async () => {
-    await getCategories()
-})
 </script>
