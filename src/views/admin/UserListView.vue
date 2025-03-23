5ce252b8234c62eb6cc974654696afe8e56e9982
rahman <rahmannurhayadi@gmail.com>
2025-03-23 11:49:25 +0700

    fix(view) : delete btnSerach compoenent

--- src/views/admin/UserListView.vue
+++ src/views/admin/UserListView.vue
@@ -1,20 +1,34 @@
 <template>
     <div class="flex flex-col gap-y-2">
-        <BtnSearch />
+        <section>
+            <div class="inline-flex w-full justify-between">
+                <div class="sm:w-full">
+                    <label class="input sm:min-w-3/4">
+                        <span><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></span>
+                        <input type="search" class="grow" placeholder="Search" />
+                    </label>
+                </div>
+
+                <button class="btn btn-primary w-24">
+                    Add
+                </button>
+            </div>
+        </section>
 
         <section>
-            <EasyDataTable :headers="headers" :items="items" :rows-items="[8,12]" :rows-per-page="8" table-class-name="customize-table"/>
+            <EasyDataTable :headers="headers" :items="items" :rows-items="[8, 12]" :rows-per-page="8"
+                table-class-name="customize-table" />
         </section>
     </div>
 </template>
 
 <script setup>
-import BtnSearch from '@/components/admin/BtnSearch.vue';
+
 const headers = [
- 
+
 ];
 
 const items = [
- 
+
 ];
 </script>
