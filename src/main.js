52b5844e1af35465874ce3097d66a2d78e5b425b
rahman <rahmannurhayadi@gmail.com>
2025-03-24 09:40:43 +0700

    setup chart js & vue-chart js

--- src/main.js
+++ src/main.js
@@ -1,40 +1,54 @@
-import './assets/css/tailwind.css'
+import "./assets/css/tailwind.css";
 
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-import { pinia } from './services/pinia'
+import { pinia } from "./services/pinia";
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
-import Vue3Toastify from 'vue3-toastify';
-import 'vue3-toastify/dist/index.css';
+import Vue3Toastify from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 
-import Vue3EasyDataTable from 'vue3-easy-data-table';
-import 'vue3-easy-data-table/dist/style.css';
-import Loading from './components/info/Loading.vue'
+import Vue3EasyDataTable from "vue3-easy-data-table";
+import "vue3-easy-data-table/dist/style.css";
+import Loading from "./components/info/Loading.vue";
 
-library.add(fas,fab,far)
+library.add(fas, fab, far);
 
-const app = createApp(App)
+const app = createApp(App);
+import {
+    Chart,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    LineController,
+    LineElement,
+    PointElement,
+} from "chart.js";
 
-app.use(Vue3Toastify,{
+Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend,LineController,LineElement,PointElement);
+
+app.use(Vue3Toastify, {
     autoClose: 1000,
-    theme : "auto" 
-})
+    theme: "auto",
+});
 
-app.component('EasyDataTable', Vue3EasyDataTable);
-app.component('LoadingOverlay',Loading)
+app.component("EasyDataTable", Vue3EasyDataTable);
+app.component("LoadingOverlay", Loading);
 
-app.component('font-awesome-icon', FontAwesomeIcon)
+app.component("font-awesome-icon", FontAwesomeIcon);
 
-app.use(pinia)
-app.use(router)
+app.use(pinia);
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
