faa6f47e1100909e01ea7d0033135610fc655be1
rahman <rahmannurhayadi@gmail.com>
2025-03-25 17:44:19 +0700

    feat(component) : fix all category

--- src/components/landingpage/CategorySection.vue
+++ src/components/landingpage/CategorySection.vue
@@ -3,9 +3,18 @@
         <h1 class="p-2 text-xl md:px-14">Categories</h1>
         <div class="w-full overflow-x-auto hide-scrollbar">
             <div class="flex flex-row gap-x-4 min-w-max p-2 justify-evenly">
+                <div class="flex items-center flex-col gap-y-2">
+                    <button class="btn btn-circle btn-ghost w-12 h-12 bg-orioles-orange text-orioles-linen hover:bg-orange-500
+                        lg:w-16 lg:h-16" type="button" @click="sendCategory(null)">
+                        <font-awesome-icon icon="fa-solid fa-earth-asia" size="lg"/>
+                    </button>
+                    <small class="text-center">
+                        All
+                    </small>
+                </div>
                 <div v-for="category in mappedCategory" class="flex items-center flex-col gap-y-2">
                     <button class="btn btn-circle btn-ghost w-12 h-12 bg-orioles-orange text-orioles-linen hover:bg-orange-500
-                        lg:w-16 lg:h-16" type="button">
+                        lg:w-16 lg:h-16" type="button" @click="sendCategory(category.id)">
                         <font-awesome-icon :icon="category.icon" size="lg" />
                     </button>
                     <small class="text-center">
@@ -33,12 +42,11 @@ const getIcon = (categoryName) => {
     if (lowerCaseName.includes("hari ibu")) return "fa-solid fa-person-dress";
     if (lowerCaseName.includes("pemakaman") || lowerCaseName.includes("duka")) return "fa-solid fa-feather-pointed";
     if (lowerCaseName.includes("custom")) return "fa-solid fa-puzzle-piece";
-    if (lowerCaseName.includes("wangi")) return "fa-solid fa-wind"; // Contoh tambahan untuk "wangi"
-    if (lowerCaseName.includes("elegant")) return "fa-solid fa-user-tie"; // Contoh tambahan untuk "wangi"
+    if (lowerCaseName.includes("wangi")) return "fa-solid fa-wind";
+    if (lowerCaseName.includes("elegant")) return "fa-solid fa-user-tie";
 
-    return "fa-solid fa-question-circle"; // Default jika tidak ditemukan
+    return "fa-solid fa-question-circle"; 
 };
-
 
 const categoryStore = useCategoryStore()
 
@@ -47,9 +55,15 @@ const { categories } = storeToRefs(categoryStore)
 const mappedCategory = computed(() => {
     return categories.value.map(item => ({
         ...item,
-        icon: getIcon(item.name) // Menambahkan ikon berdasarkan kategori
+        icon: getIcon(item.name)
     }));
 })
+
+const emits = defineEmits(['sendCategoryId'])
+
+const sendCategory = (id)=>{
+    return emits('sendCategoryId',id)
+}
 
 onMounted(async () => {
     await categoryStore.getCategories()
