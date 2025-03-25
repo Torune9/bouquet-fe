f4e011ef7114d7279d8d7af66f764ed956ba4a3c
rahman <rahmannurhayadi@gmail.com>
2025-03-25 11:29:17 +0700

    fix(component) : fix product data in product card

--- src/components/card/ProductCard.vue
+++ src/components/card/ProductCard.vue
@@ -2,20 +2,21 @@
     <div>
         <RouterLink :to="{
             name: 'detail-bouquet', params: {
-                id: id
+                id: bouquet.id
             }
         }">
             <div
                 class="flex flex-col w-44 shadow-md relative cursor-pointer hover:bg-silver/10 hover:scale-95 transition duration-300 md:w-52 lg:w-56">
                 <div class="h-36 md:h-40 lg:h-48">
-                    <img :src="img" alt="product-image" class="w-full h-full object-cover">
+                    <img :src="imageDefault(bouquet.ImageBouquets) " alt="product-image" class="w-full h-full object-cover">
                 </div>
                 <div class="p-2">
-                    <h1 class="text-sm">
+                    <h1 class="text-sm md:text-md">
 
-                        {{ name.length > 20 ? `${name.slice(0,18)}...` : name }}
+                        {{ bouquet.name.length > 20 ? `${bouquet.name.slice(0,18)}...` : bouquet.name }}
                     </h1>
-                    <h2 class="text-lg font-semibold mt-1">{{ idrFormatter(price) }}</h2>
+                    <h2 class="text-lg font-semibold mt-1">{{ formatToIdr(bouquet.price) }}</h2>
+                    <small><span>Stock : </span>{{ bouquet.stock }}</small>
                 </div>
                 <div class="p-2 flex gap-2 justify-end text-sm">
                     <h1 class="text-center border w-12 rounded-sm">
@@ -31,29 +32,15 @@
 </template>
 
 <script setup>
+import { formatToIdr } from '@/services/formatter';
+
 const props = defineProps({
-    id: {
-        type: Number,
-        default: 0
-    },
-    img: {
-        type: String,
-        default: 'Bouquet image'
-    },
-    name: {
-        type: String,
-        default: 'Bouquet Name'
-    },
-    price: {
-        type: Number,
-        default: 0
-    },
+    bouquet : {
+        type : Object
+    }
 })
 
-const idrFormatter = (number) => {
-    return new Intl.NumberFormat('id-ID', {
-        style: 'currency',
-        currency: 'IDR'
-    }).format(number)
+const imageDefault = (image) => {
+    return image.length > 0 ? image[0].path : 'https://placehold.co/600x400?text=Bouquet Image'
 }
 </script>
