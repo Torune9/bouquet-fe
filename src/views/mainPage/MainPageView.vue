e41b40bac9af50a40b108c628cd8f84b68378652
rahman <rahmannurhayadi@gmail.com>
2025-03-25 17:44:58 +0700

    fix(view) : fix category filter

--- src/views/mainPage/MainPageView.vue
+++ src/views/mainPage/MainPageView.vue
@@ -5,9 +5,9 @@
 
         <MarqueeComponent/>
 
-        <CategorySection class="mt-4"/>
+        <CategorySection class="mt-4" @send-category-id="getCategory"/>
 
-        <ProductSection class="mt-4"/>
+        <ProductSection class="mt-4" :category="id"/>
     </template>
    </MainLayout>
 </template>
@@ -19,4 +19,9 @@ import MarqueeComponent from '@/components/info/MarqueeComponent.vue';
 import HeroSection from '@/components/landingpage/HeroSection.vue';
 import CategorySection from '@/components/landingpage/CategorySection.vue';
 import ProductSection from '@/components/landingpage/ProductSection.vue';
+import { ref } from 'vue';
+const id = ref('')
+const getCategory = (category)=>{
+    return id.value = category
+}
 </script>
