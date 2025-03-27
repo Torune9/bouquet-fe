a8967e764b8b69feec679b0344880370316a0464
rahman <rahmannurhayadi@gmail.com>
2025-03-27 17:19:44 +0700

    fix(view) : fix eror handling and toast success in main page

--- src/views/mainPage/MainPageView.vue
+++ src/views/mainPage/MainPageView.vue
@@ -1,15 +1,15 @@
 <template>
-   <MainLayout>
-    <template #content>
-        <HeroSection/>
+    <MainLayout>
+        <template #content>
+            <HeroSection />
 
-        <MarqueeComponent/>
+            <MarqueeComponent />
 
-        <CategorySection class="mt-4" @send-category-id="getCategory"/>
+            <CategorySection class="mt-4" @send-category-id="getCategory" />
 
-        <ProductSection class="mt-4" :category="id"/>
-    </template>
-   </MainLayout>
+            <ProductSection class="mt-4" :category="id" />
+        </template>
+    </MainLayout>
 </template>
 
 <script setup>
@@ -19,9 +19,19 @@ import MarqueeComponent from '@/components/info/MarqueeComponent.vue';
 import HeroSection from '@/components/landingpage/HeroSection.vue';
 import CategorySection from '@/components/landingpage/CategorySection.vue';
 import ProductSection from '@/components/landingpage/ProductSection.vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
+import { toast } from 'vue3-toastify';
 const id = ref('')
-const getCategory = (category)=>{
+const getCategory = (category) => {
     return id.value = category
+}
+
+onMounted(() => { })
+if (history.state.isSuccess) {
+    toast.success('Login success', {
+        onClose: () => {
+            history.replaceState(null, '')
+        }
+    })
 }
 </script>
